1a3cc381e95a854a11e2bc956741c653447b7dbe
Mau <79354255+Luceium@users.noreply.github.com>
2023-11-17 21:28:32 -0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+A tool to help you find and share your availability.
+This project was created in response to filling out to many when2meet's and wanting an easier way to share interview availability.
+
+# How to use
+
+### CLI
+
+### Web view
+
+# TODOs
+
+- Create MVP
+- Create CLI
+- Create Web view
+- Finish README
---
+++ main.py
@@ -0,0 +1,19 @@
+def getICSFile():
+    # TODO API call to get ICS file
+    print("hi")
+
+def getEvents(icsFile):
+    # TODO
+    print("hi")
+
+def setRange(start, end, events):
+    # TODO
+    print("hi")
+
+def getAvalibility(events, length, startTime=0, endTime=24, padding=0, startPadding=0, endPadding=0):
+    # TODO
+    print("hi")
+
+icsFile = getICSFile()
+events = getEvents(icsFile)
+events = setRange("2023-11-28", "2023-12-15", events)
